23abdfbafea994452129c3da0387d5f7e388df22
youta777 <yousoccer07@yahoo.co.jp>
2020-07-17 00:43:58 +0900

    ABC172のA問題を追加

---
+++ ABC/ABC172/a.rb
@@ -0,0 +1,20 @@
+=begin
+
+  date：2020.7.17 fri
+  result：AC
+  
+  問題URL：https://atcoder.jp/contests/abc172/tasks/abc172_a
+  結果URL：https://atcoder.jp/contests/abc172/submissions/15276049
+
+=end
+
+# 自分の解答 ------------------------
+a = gets.to_i
+
+puts a + a*a + a*a*a
+
+
+# 他の回答 -------------------------
+a = gets.chomp.to_i
+
+puts a + a**2 + a**3
